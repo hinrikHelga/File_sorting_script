438b8f6ef72ee7919be90a915dd1b4436f71e625
Hinrik <hinrik.helga@gmail.com>
2018-12-12 15:40:58 +0000

    Regex patterns now in list

--- clean.py
+++ clean.py
@@ -3,18 +3,45 @@ import sys, glob, shutil, os, re
 def main():
     downloads = sys.argv[1]
     #structured = sys.argv[2]
+    regex_tvs_file = tv_show_patterns_file()
 
-    # Work in progress...
-    regex_tv_shows_pattern = re.compile(r"([Ss]{1}[0-9]{2}[Ee]{1}[0-9]{2})|(Season)\s?([0-9]{1,2})\s*[\-]*\s*(Episode)\s*([0-9]{1,2})")
-    # TODO: regex for movies
-    
     # Traverse recursively through root directory
-    traverse_root(downloads, regex_tv_shows_pattern)
-
-    # TODO: structured directory functionality
+    traverse_root(downloads, regex_tvs_file)
 
 
-def traverse_root(folder_structure, regex):
+def tv_show_patterns_file():
+    # Accepts S01E05, s12e21
+    regex_tv_shows_pattern1 = re.compile(r"([Ss]{1}[0-9]{1,2}|[Ee]{1}[0-9]{1,2})")
+
+    # Accepts Season 2 - Episode 01 where Season, 2, Episode and 01 are all different groups (group 1, 2, 3 and 4)
+    regex_tv_shows_pattern2 = re.compile(r"(Season)\s?([0-9]{1,2})\s*[\-]*\s*(Episode)\s*([0-9]{1,2})")
+
+    # Accpets 205.avi, 205b, 0100 (bugs needs to be fixes such as x264)
+    regex_tv_shows_pattern3 = re.compile(r"([0-9]{3,4}[a|b]?)")
+
+    # Accepts 30 Rock [1.01] Pilot.avi
+    regex_tv_shows_pattern4 = re.compile(r"(\[[0-9]{1,2}\.[0-9]{1,2}\])")
+
+    # Accepts 01x01, 1x01
+    regex_tv_shows_pattern5 = re.compile(r"([0-9]{1,2}x[0-9]{1,2})")
+
+    # Accepts hdtv, HDTV, has bugs so not included in list
+    regex_tv_shows_pattern6 = re.compile(r"(hdtv|HDTV)")
+
+    regex_patterns = [regex_tv_shows_pattern1,
+                    regex_tv_shows_pattern2,
+                    regex_tv_shows_pattern3, 
+                    regex_tv_shows_pattern4,
+                    regex_tv_shows_pattern5]
+    return regex_patterns
+
+
+# TODO: regex for movies
+# TODO: regex for TV show folders
+# TODO: regex for Movie folders
+
+
+def traverse_root(folder_structure, regex_tvs_file):
     for root, dirs, files in os.walk(folder_structure):
         try:
             path = root.split(os.sep)
@@ -26,8 +53,9 @@ def traverse_root(folder_structure, regex):
             try:
                 extension = file.split(".")[-1]
                 if (validate_extension(extension)):
-                    if regex.findall(file):
-                        print(len(path) * '---', file)
+                    for pattern in regex_tvs_file:
+                        if pattern.findall(file):
+                            print(len(path) * '---', file)
             except UnicodeEncodeError:
                 pass
 
@@ -40,5 +68,7 @@ def validate_extension(f_extension):
         return True
     
     return False
+
+# TODO: structured directory functionality
 
 print(main())
